b4980c7fca880fe34ee9673be919f47d18004109
qu1ck <anlutsenko@gmail.com>
2019-10-30 17:04:03 -0700

    Change order of priority for heuristic to find net/xml
    Don't stop on first found file in a directory
    
    Fixes #115

--- InteractiveHtmlBom/ecad/kicad_extra/__init__.py
+++ InteractiveHtmlBom/ecad/kicad_extra/__init__.py
@@ -26,26 +26,26 @@ def find_latest_schematic_data(base_name, directories):
     :param directories: list of directories to search
     :return: last modified parsable file path or None if not found
     """
-    for dir in directories:
-        f = _find_in_dir(base_name, dir)
-        if f:
-            return f
-    return None
-
-
-def _find_in_dir(base_name, dir):
-    _, _, files = next(os.walk(dir), (None, None, []))
-    # filter out files that we can not parse
-    files = [f for f in files if os.path.splitext(f)[1] in PARSERS.keys()]
-    # get their modification time and sort in descending order
-    files = [(os.path.getmtime(os.path.join(dir, f)), f) for f in files]
+    files = []
+    for d in directories:
+        files.extend(_find_in_dir(d))
+    # sort by decreasing modification time
     files = sorted(files, reverse=True)
     if files:
         # try to find first (last modified) file that has name matching pcb file
         for _, f in files:
-            if os.path.splitext(f)[0] == base_name:
-                return os.path.join(dir, f)
+            if os.path.splitext(os.path.basename(f))[0] == base_name:
+                return f
         # if no such file is found just return last modified
-        return os.path.join(dir, files[0][1])
+        return files[0][1]
     else:
         return None
+
+
+def _find_in_dir(dir):
+    _, _, files = next(os.walk(dir), (None, None, []))
+    # filter out files that we can not parse
+    files = [f for f in files if os.path.splitext(f)[1] in PARSERS.keys()]
+    files = [os.path.join(dir, f) for f in files]
+    # get their modification time and sort in descending order
+    return [(os.path.getmtime(f), f) for f in files]
