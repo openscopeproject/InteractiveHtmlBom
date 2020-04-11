f346175e3ed5d80da49c9fdac70e1b59514081b3
scarrrr316 <43269183+scarrrr316@users.noreply.github.com>
2020-04-11 13:16:30 +0800

    Create Run.bat for EasyEDA (#140)
    Create Run.bat for EasyEDA
    
    dont need to open cmd , just double click this batch file and drag the EasyEDA source file

---
+++ InteractiveHtmlBom/Run.bat
@@ -0,0 +1,35 @@
+@echo off
+
+echo -------------------------------------------------------------------------------------------------------------------
+echo -------------------------------------------------------------------------------------------------------------------
+echo                                                                                                                                                                                    -
+echo                                  Thank you for using InteractiveHtmlBom
+echo                           https://github.com/openscopeproject/InteractiveHtmlBom
+echo                                         Bat file by Scarrrr0725
+echo                                                                                                                                                                                    -
+echo --------------------------------------------------------------------------------------------------------------------
+echo --------------------------------------------------------------------------------------------------------------------
+
+::delete --show-dialog after frist start up and setting
+set option=--show-dialog
+
+set FilePath=%~dp0
+set pyFilePath=%FilePath%generate_interactive_bom.py
+
+:convert
+set /p pathofEDASourceFile=Please Drag the EasyEDA PCB source file here :
+
+echo  Converting. . . . . . . . . .
+python %pyFilePath% %pathofEDASourceFile% %option%
+
+echo -------------------------------------------------------------------------------------------------------------------
+echo -------------------------------------------------------------------------------------------------------------------
+echo                                                                                                                                                                                    -
+echo                                 EDA source file is converted to bom successfully!
+echo                                                                                                                                                                                    -
+echo -------------------------------------------------------------------------------------------------------------------
+echo -------------------------------------------------------------------------------------------------------------------
+
+CHOICE /C YN /N /M "Do you want to convert another file? [Y/N"
+	if errorlevel 2 exit
+	if errorlevel 1 goto convert
