9fa6e957e6289b1de3d62481bcfccc22f7a5bd21
scarrrr316 <43269183+scarrrr316@users.noreply.github.com>
2020-04-11 14:21:56 +0800

    add chinese language

--- InteractiveHtmlBom/Run.bat
+++ InteractiveHtmlBom/Run.bat
@@ -1,14 +1,7 @@
 @echo off
 
-echo -------------------------------------------------------------------------------------------------------------------
-echo -------------------------------------------------------------------------------------------------------------------
-echo                                                                                                                                                                                    -
-echo                                  Thank you for using InteractiveHtmlBom
-echo                           https://github.com/openscopeproject/InteractiveHtmlBom
-echo                                         Bat file by Scarrrr0725
-echo                                                                                                                                                                                    -
-echo --------------------------------------------------------------------------------------------------------------------
-echo --------------------------------------------------------------------------------------------------------------------
+language=en 
+::supported language :English and Chinese-Simplified
 
 ::delete --show-dialog after frist start up and setting
 set option=--show-dialog
@@ -16,7 +9,13 @@ set option=--show-dialog
 set FilePath=%~dp0
 set pyFilePath=%FilePath%generate_interactive_bom.py
 
-:convert
+if language==en (
+	call :convert_en 
+
+)
+
+
+:convert_en
 set /p pathofEDASourceFile=Please Drag the EasyEDA PCB source file here :
 
 echo  Converting. . . . . . . . . .
@@ -30,6 +29,48 @@ echo                                                                            
 echo -------------------------------------------------------------------------------------------------------------------
 echo -------------------------------------------------------------------------------------------------------------------
 
-CHOICE /C YN /N /M "Do you want to convert another file? [Y/N"
+CHOICE /C YN /N /M "Do you want to convert another file? [Y/N]
 	if errorlevel 2 exit
-	if errorlevel 1 goto convert
+	if errorlevel 1 goto convert_en
+
+
+:convert_zh
+set /p pathofEDASourceFile=请将您的EDA PCB源文件拖移至此:
+
+echo  导出中. . . . . . . . . .
+python %pyFilePath% %pathofEDASourceFile% %option%
+
+echo -------------------------------------------------------------------------------------------------------------------
+echo -------------------------------------------------------------------------------------------------------------------
+echo .                                                                                                                                                                                   
+echo                                           您的EDA 源文件已成功转换到 Bom ！
+echo .                                                                                                                                                                        
+echo -------------------------------------------------------------------------------------------------------------------
+echo -------------------------------------------------------------------------------------------------------------------
+
+CHOICE /C YN /N /M "请问您还要为其他文件导出BOM吗? [Y/N]
+	if errorlevel 2 exit
+	if errorlevel 1 goto convert_zh
+
+
+:startupEcho_en
+echo -------------------------------------------------------------------------------------------------------------------
+echo -------------------------------------------------------------------------------------------------------------------
+echo .                                                                                                                                                                                  -
+echo                                  Thank you for using InteractiveHtmlBom
+echo                           https://github.com/openscopeproject/InteractiveHtmlBom
+echo                                         Bat file by Scarrrr0725
+echo .                                                                                                                                                                                  -
+echo --------------------------------------------------------------------------------------------------------------------
+echo --------------------------------------------------------------------------------------------------------------------
+
+:startupEcho_zh
+echo -------------------------------------------------------------------------------------------------------------------
+echo -------------------------------------------------------------------------------------------------------------------
+echo .                                                                                                                                                  
+echo                                          感谢使用 Generate Interactive Bom                                                       
+echo                                                   Powered By Qu1ck                                                   
+echo                                    Bat ( EasyEDA）版本 : Powered By Scarrrr0725
+echo .                                                                                                                                                                 
+echo --------------------------------------------------------------------------------------------------------------------
+echo --------------------------------------------------------------------------------------------------------------------
