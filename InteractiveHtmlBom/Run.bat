202f1d34cd3fe47c59cdf4d227af0eb7c0f5083e
scarrrr316 <43269183+scarrrr316@users.noreply.github.com>
2020-12-05 22:17:07 +0800

    Deleted Chinese language (#153)
    too many bugs when bat file run with  Chinese

--- InteractiveHtmlBom/Run.bat
+++ InteractiveHtmlBom/Run.bat
@@ -5,16 +5,8 @@ set FilePath=%~dp0
 ::delete --show-dialog after frist start up and setting
 set option=--show-dialog
 
-::detect current language of user.
-FOR /F "tokens=3" %%a IN ('reg query "HKCU\Control Panel\Desktop" /v PreferredUILanguages ^| find "PreferredUILanguages"') DO set language=%%a
-set language=%language:~,2%
-if %language%==zh (
-	set PYTHONIOENCODING=utf-8
-	chcp 65001
-	call %FilePath%\i18n\language_zh.bat
-) else (
-	call %FilePath%\i18n\language_en.bat
-)
+call %FilePath%\i18n\language_en.bat
+
 echo -------------------------------------------------------------------------------------------------------------------
 echo -------------------------------------------------------------------------------------------------------------------
 echo                                                                                                                                                                                    -
--- InteractiveHtmlBom/i18n/language_en.bat
+++ InteractiveHtmlBom/i18n/language_en.bat
@@ -1,7 +1,7 @@
 ::start up echo
-set i18n_gitAddr=                     https://github.com/openscopeproject/InteractiveHtmlBom
-set i18n_batScar=                                  Bat file by Scarrrr0725
-set i18n_thx4using=                     Thankyou For Using Generate Interactive Bom
+set i18n_gitAddr=                                 https://github.com/openscopeproject/InteractiveHtmlBom
+set i18n_batScar=                                                Bat file by Scarrrr0725
+set i18n_thx4using=                                       Thankyou For Using Generate Interactive Bom
 
 ::convert
 set i18n_draghere=Please Drag the EasyEDA PCB source file here :
@@ -9,4 +9,4 @@ set i18n_converting=Converting . . . . . .
 
 ::converted
 set i18n_again=Do you want to convert another file ? 
-set i18n_converted=                     EDA source file is converted to bom successfully!
+set i18n_converted=                                    EDA source file is converted to bom successfully!
