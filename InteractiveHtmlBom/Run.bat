889c5f4aed4cae4bd4ace233f6dc04603bf3c590
XiaoXi <admin@soraharu.com>
2021-10-01 21:27:56 +0800

    fix: look for language settings in two registry locations

--- InteractiveHtmlBom/Run.bat
+++ InteractiveHtmlBom/Run.bat
@@ -6,12 +6,30 @@ set FilePath=%~dp0
 set option=--show-dialog
 
 ::detect current language of user.
+reg query "HKCU\Control Panel\Desktop" /v PreferredUILanguages>nul 2>nul&&goto _dosearch1_||goto _dosearch2_
+
+:_dosearch1_
+FOR /F "tokens=3" %%a IN (
+	'reg query "HKCU\Control Panel\Desktop" /v PreferredUILanguages ^| find "PreferredUILanguages"'
+) DO (
+	set language=%%a
+)
+set language=%language:~,2%
+goto _setlanguage_
+
+:_dosearch2_
 FOR /F "tokens=3" %%a IN (
 	'reg query "HKLM\SYSTEM\ControlSet001\Control\Nls\Language" /v InstallLanguage ^| find "InstallLanguage"'
 ) DO (
 	set language=%%a
 )
 if %language%==0804 (
+	set language=zh
+)
+goto _setlanguage_
+
+:_setlanguage_
+if %language%==zh (
 	call %FilePath%\i18n\language_zh.bat
 ) else (
 	call %FilePath%\i18n\language_en.bat
@@ -31,7 +49,7 @@ echo ---------------------------------------------------------------------------
 
 set pyFilePath=%FilePath%generate_interactive_bom.py
 
-:convert
+:_convert_
 if not defined pathofEDASourceFile (
 	set /p pathofEDASourceFile=%i18n_draghere%
 )
@@ -52,4 +70,4 @@ echo ---------------------------------------------------------------------------
 
 CHOICE /C YN /N /M "%i18n_again% [ Y/N ]"
 	if errorlevel 2 exit
-	if errorlevel 1 goto convert
+	if errorlevel 1 goto _convert_
