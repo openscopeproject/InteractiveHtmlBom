9c1f97ceed9c3cf070f68ea56e34953dc819a5e4
Mark Hämmerling <dev@markh.de>
2022-12-14 18:37:36 +0100

    Relax URL-matching regex
    As suggested by @qu1ck, use
    
    https://gitlab.com/kicad/code/kicad/-/blob/2f8cc845513b918e219f32733a210bf458138f91/kicad/pcm/schemas/pcm.v1.schema.json#L287
    
    as a reference, but fix these issues:
    
    * remove the single period (any char) in the http(s) part to allow
    single-character host names, such as http://a (valid, brings up the
    default page of the webserver on host 'a')
    
    * add missing parenthesis (group) around everything between ^ and $ for
    the alternative (|) to work as expected (the original regex will match
    file:// in the middle of the string, as the regex matches anything that
    *either* *begins* with http(s):// *or* *ends* with a file://... URL
    scheme)

--- InteractiveHtmlBom/web/ibom.js
+++ InteractiveHtmlBom/web/ibom.js
@@ -660,8 +660,8 @@ function populateBomBody(placeholderColumn = null, placeHolderElements = null) {
           references.map(r => r[1]).forEach((id) => valueSet.add(pcbdata.bom.fields[id][field_index]));
           td = document.createElement("TD");
           var output = new Array();
-          const urlRegex = /^((file|https?):\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*))$/g;
           for (let item of Array.from(valueSet)) {
+          const urlRegex = /^(https?:\/\/[^\s\/$.?#][^\s]*|file:\/\/([a-zA-Z]:|\/)[^\x00]+)$/;
             const visible = highlightFilter(item);
             if (item.match(urlRegex)) {
               output.push('<a href="' + item + '" target="_blank">' + visible + '</a>');
