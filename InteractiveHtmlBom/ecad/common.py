b94e8d9f49deb9998bea4b2d3ce42f6634ac25ab
zharov.d.v <gandalf@sinnfein.ru>
2025-01-25 16:17:28 +0300

    Fix BB computing for arc (typo in xc/yc, forgot radius)

--- InteractiveHtmlBom/ecad/common.py
+++ InteractiveHtmlBom/ecad/common.py
@@ -122,10 +122,10 @@ class EcadParser(object):
                 a1 = drawing['startangle']
                 a2 = drawing['endangle']
                 r = drawing['radius']
-                x1 = xc + math.cos(math.radians(a1))
-                y1 = xc + math.sin(math.radians(a1))
-                x2 = xc + math.cos(math.radians(a2))
-                y2 = xc + math.sin(math.radians(a2))
+                x1 = xc + r * math.cos(math.radians(a1))
+                y1 = yc + r * math.sin(math.radians(a1))
+                x2 = xc + r * math.cos(math.radians(a2))
+                y2 = yc + r * math.sin(math.radians(a2))
                 da = a2 - a1 if a2 > a1 else a2 + 360 - a1
                 la = 1 if da > 180 else 0
                 svgpath = 'M %s %s A %s %s 0 %s 1 %s %s' % \
